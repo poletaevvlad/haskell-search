5c3c167b23915f2286c6ab5e6dca4bf1cd292f24
Vlad Poletaev <poletaev.vladislav@gmail.com>
2019-10-28 23:03:35 +0300

    Implemented splitting string into words

--- src/TextUtils/Processing.hs
+++ src/TextUtils/Processing.hs
@@ -1,4 +1,4 @@
-module TextUtils.Processing (getExcerpt) where
+module TextUtils.Processing (getExcerpt, splitWords) where
 
 import Data.Char
 
@@ -18,3 +18,18 @@ getExcerpt len text = nonLetters ++ word ++ (getExcerpt (len - 1) rest)
           let (v, next) = dropWhileBoth predicate cs
           in (c:v, next)
       | otherwise = ([], string)
+
+
+splitWords :: String -> [String]
+splitWords text = filter (not . null) $ reverse $ helper [""] text
+  where
+    isWordSeparator :: Char -> Bool
+    isWordSeparator char = isSeparator char || isPunctuation char
+
+    helper :: [String] -> String -> [String]
+    helper [] _ = error "illegal state"
+    helper (curr:pr) [] = reverse curr:pr
+    helper (curr:pr) (c:cs)
+      | c == '\'' = helper (curr:pr) cs
+      | isWordSeparator c = helper ("":reverse curr:pr) cs
+      | otherwise = helper ((c:curr):pr) cs
--- tests/TextUtils/ProcessingSpec.hs
+++ tests/TextUtils/ProcessingSpec.hs
@@ -13,3 +13,12 @@ spec = do
     it "should return part of a string" $ do
       getExcerpt 3 "Lorem ipsum dolor sit amet" `shouldBe` "Lorem ipsum dolor"
 
+  describe "splitWords" $ do
+    it "should return null list if the string is empty" $ do
+      splitWords "" `shouldBe` []
+    it "should remove spaces and punctuation"  $ do
+      splitWords "hello, world" `shouldBe` ["hello", "world"]
+    it "should preserve apostrophy" $ do
+      splitWords "shouldn't don't" `shouldBe` ["shouldnt", "dont"]
+    it "should ignore trailing and leading spaces and punctuation marks" $ do
+      splitWords "  hello --" `shouldBe` ["hello"]
