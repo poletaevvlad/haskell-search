81873adb134f7002db36c66ab3c6835cb74b696a
Vlad Poletaev <poletaev.vladislav@gmail.com>
2019-10-31 15:37:51 +0300

    Utility function for setting page number for the Url.

--- src/Pages/UrlUtils.hs
+++ src/Pages/UrlUtils.hs
@@ -1,4 +1,7 @@
-module Pages.UrlUtils(popUrlComponent) where
+module Pages.UrlUtils(popUrlComponent, setPageNum) where
+
+import Data.Char(isDigit)
+import Data.List(isPrefixOf)
 
 popUrlComponent :: String -> String
 popUrlComponent "/" = "/"
@@ -11,3 +14,28 @@ popUrlComponent url =
     mapLastComponent [_] = [False]
     mapLastComponent ('/': xs) = True: mapLastComponent xs
     mapLastComponent (_: xs) = let rest = mapLastComponent xs in (head rest): rest
+
+
+setPageNum :: String -> Int -> String
+setPageNum uri pageNum =
+  let result = removedCurrentPage ++ pageNumComponent
+  in if null result then "/" else result
+  where
+    removeSlash str = if head str == '/' then tail str else str
+
+    skipNumbers "" = Nothing
+    skipNumbers str =
+      if isDigit $ head str then Just $ dropWhile isDigit str else Nothing
+
+    removeNumPrefix "" = Nothing
+    removeNumPrefix str =
+      if "-egap" `isPrefixOf` str then Just $ drop 5 str else Nothing
+
+    removedCurrentPage =
+      let
+        reversed = removeSlash $ reverse uri
+        path = removeSlash <$> (skipNumbers reversed >>= removeNumPrefix)
+      in case path of
+        Nothing -> reverse reversed
+        Just t -> reverse t
+    pageNumComponent = if pageNum == 1 then "" else "/page-" ++ show pageNum
--- tests/Pages/UrlUtilsSpec.hs
+++ tests/Pages/UrlUtilsSpec.hs
@@ -17,3 +17,20 @@ spec = do
       popUrlComponent "/a/b/c" `shouldBe` "/a/b"
     it "should pop components with trailing slash" $ do
       popUrlComponent "/a/b/c/" `shouldBe` "/a/b"
+  describe "setPageNum" $ do
+    it "should add page num to Url" $ do
+      setPageNum "/path" 4 `shouldBe` "/path/page-4"
+    it "should change page number" $ do
+      setPageNum "/path/page-2" 3 `shouldBe` "/path/page-3"
+    it "should remove page number from Url" $ do
+      setPageNum "/path/page-2" 1 `shouldBe` "/path"
+    it "should correctly add page number to root" $ do
+      setPageNum "/" 2 `shouldBe` "/page-2"
+    it "should correctly remove page number for root" $ do
+      setPageNum "/page-3" 1 `shouldBe` "/"
+    it "should ignore trailing slash when adding" $ do
+      setPageNum "/page/" 2 `shouldBe` "/page/page-2"
+    it "should ignore trailing slash when removing" $ do
+      setPageNum "/page/page-3/" 1 `shouldBe` "/page"
+    it "should ignore trailing slash when changing" $ do
+      setPageNum "/page/page-3/" 4 `shouldBe` "/page/page-4"
