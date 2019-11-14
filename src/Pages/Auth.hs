a4a665c918d4c13c1655b5f86662a14a7eb50d7f
Vlad Poletaev <poletaev.vladislav@gmail.com>
2019-11-15 01:08:21 +0300

    Implemented auth security verification.

--- src/Pages/Auth.hs
+++ src/Pages/Auth.hs
@@ -1,16 +1,21 @@
-module Pages.Auth (Token(Token), TokenStruct(TokenStruct),
-  AuthSecret(AuthSecret)) where
+module Pages.Auth (AuthConf(..), Token(Token), TokenStruct(TokenStruct),
+  AuthSecret(AuthSecret), validateAuthSecret) where
 
 import Data.ByteString.Lazy (ByteString)
+import qualified Data.ByteString as BSStrict
 import qualified Data.ByteString.Lazy as ByteString
 import Data.Binary (Binary)
 import qualified Data.Binary as Binary
-
+import Data.Time (nominalDiffTimeToSeconds)
+import Data.Time.Clock (UTCTime, NominalDiffTime, addUTCTime,
+  secondsToNominalDiffTime)
+import Data.Time.Clock.POSIX (utcTimeToPOSIXSeconds, posixSecondsToUTCTime)
+import Crypto.Cipher.AES (decryptCBC, initAES)
+import Data.Maybe (isJust)
 
 data AuthConf =
-  AuthConf { auConfTimeOut :: Int
-           , auConfSecret :: ByteString
-           }
+  AuthConf { auConfTimeOut :: NominalDiffTime
+           , auConfSecret :: ByteString }
 
 
 data Token = Token ByteString deriving (Show, Eq)
@@ -25,8 +30,11 @@ instance Binary Token where
           readString 0 = return []
           readString n = (:) <$> Binary.get <*> readString (n - 1)
 
+instance Binary UTCTime where
+  put time = Binary.put $ (floor $ nominalDiffTimeToSeconds $ utcTimeToPOSIXSeconds time :: Binary.Word64)
+  get = (posixSecondsToUTCTime . secondsToNominalDiffTime . fromIntegral) <$> (Binary.get :: Binary.Get Binary.Word64)
 
-data TokenStruct = TokenStruct Binary.Word64 Token deriving (Show, Eq)
+data TokenStruct = TokenStruct UTCTime Token deriving (Show, Eq)
 
 instance Binary TokenStruct where
   put (TokenStruct time token) = Binary.put time >> Binary.put token
@@ -38,3 +46,37 @@ data AuthSecret = AuthSecret Token ByteString deriving (Show, Eq)
 instance Binary AuthSecret where
   put (AuthSecret token encrypted) = Binary.put token >> Binary.put encrypted
   get = (AuthSecret) <$> Binary.get <*> Binary.get
+
+
+-- Validating auth secret
+
+maybeDecode :: (Binary m) => ByteString -> Maybe m
+maybeDecode string =
+  case Binary.decodeOrFail string of
+    Left _ -> Nothing
+    Right (_, _, x) -> Just x
+
+maybeDecrypt :: AuthConf -> ByteString -> Maybe ByteString
+maybeDecrypt conf message = do
+  let len = ByteString.length message
+  validIf $ len > 16
+  validIf $ len `mod` 16 == 0
+
+  let aes = initAES $ ByteString.toStrict $ auConfSecret conf
+  let strict = ByteString.toStrict message
+  let iv = BSStrict.take 16 strict
+  let ciphertext = BSStrict.drop 16 strict
+  return $ ByteString.fromStrict $ decryptCBC aes iv ciphertext
+
+validIf :: Bool -> Maybe ()
+validIf True = Just ()
+validIf False = Nothing
+
+
+validateAuthSecret :: AuthConf -> UTCTime -> ByteString -> Bool
+validateAuthSecret conf currentTime secretString = isJust $ do
+  (AuthSecret token encrypted) <- maybeDecode secretString
+  (TokenStruct time token2) <- maybeDecrypt conf encrypted >>= maybeDecode
+  validIf $ token == token2
+  validIf $ currentTime > time
+  validIf $ (addUTCTime (auConfTimeOut conf) time) < currentTime
--- tests/Pages/AuthSpec.hs
+++ tests/Pages/AuthSpec.hs
@@ -1,10 +1,34 @@
 module Pages.AuthSpec (spec) where
 
+import Prelude hiding (length)
 import Test.Hspec
 import Pages.Auth
-import Data.ByteString.Lazy
+import Data.ByteString.Lazy hiding (repeat, take)
 import qualified Data.Binary as B
+import qualified Data.Time.Clock as Clock
+import Data.Time.Clock.POSIX
+import qualified Crypto.Cipher.AES as AES
+import Data.Binary(Binary)
 
+
+key :: ByteString
+key = pack [0x75, 0x77, 0x21, 0x7A, 0x25, 0x43, 0x2A, 0x46,
+            0x2D, 0x4A, 0x61, 0x4E, 0x64, 0x52, 0x67, 0x55,
+            0x6B, 0x58, 0x70, 0x32, 0x73, 0x35, 0x76, 0x38,
+            0x79, 0x2F, 0x41, 0x3F, 0x44, 0x28, 0x47, 0x2B]
+
+iv :: ByteString
+iv = pack [16..31]
+
+
+pad :: ByteString -> ByteString
+pad string =
+  mconcat [string, pack $ take (fromIntegral $ 16 - (length string `mod` 16)) $ repeat (0 :: B.Word8)]
+
+
+encrypt :: (Binary b) => b -> ByteString
+encrypt v = let aes = AES.initAES $ toStrict key
+            in fromStrict $ AES.encryptCBC aes (toStrict iv) $ toStrict $ pad $ B.encode v
 
 testBinary :: (B.Binary m, Show m, Eq m) => m -> ByteString -> SpecWith ()
 testBinary obj bytes = do
@@ -14,6 +38,12 @@ testBinary obj bytes = do
     B.decode bytes `shouldBe` obj
 
 
+addTime :: Clock.UTCTime -> Int -> Clock.UTCTime
+addTime time diff =
+  let timeDiff = Clock.secondsToNominalDiffTime $ fromIntegral diff
+  in Clock.addUTCTime timeDiff time
+
+
 spec :: Spec
 spec = do
   let tokenString = pack [1..32]
@@ -21,7 +51,8 @@ spec = do
 
   describe "Binary TokenStruct" $ do
     let tokenStructString = mconcat [pack [0x12, 0x34, 0x56, 0x78, 0x9a, 0xbc, 0xde, 0xf0], tokenString]
-    let tokenStruct = TokenStruct 0x123456789abcdef0 $ Token tokenString
+    let time = posixSecondsToUTCTime $ fromIntegral 0x123456789abcdef0
+    let tokenStruct = TokenStruct time $ Token tokenString
     testBinary tokenStruct tokenStructString
 
   describe "Binary AuthSecret" $ do
@@ -29,3 +60,37 @@ spec = do
     let encoded = mconcat [tokenString, B.encode (4 :: B.Word64), bs]
     let decoded = AuthSecret (Token tokenString) bs
     testBinary decoded encoded
+
+  describe "validateAuthSecret" $ do
+    let conf = AuthConf { auConfTimeOut = 5000, auConfSecret = key }
+
+    it "should not validate if string is not a valid secret object" $ do
+      time <- Clock.getCurrentTime
+      validateAuthSecret conf time (pack [0x12, 0x13, 0x14]) `shouldBe` False
+    it "should not validate if IV is too short" $ do
+      time <- Clock.getCurrentTime
+      let message = mconcat [tokenString, B.encode (3 :: B.Word64), pack [0x12, 0x13, 0x14]]
+      validateAuthSecret conf time message `shouldBe` False
+    it "should not validate if encrypted message is too short" $ do
+      time <- Clock.getCurrentTime
+      let message = mconcat [tokenString, B.encode (19 :: B.Word64), iv, pack [0x12, 0x13, 0x14]]
+      validateAuthSecret conf time message `shouldBe` False
+    it "should not validate if encrypted message is not a valid string" $ do
+      time <- Clock.getCurrentTime
+      let message = mconcat [tokenString, B.encode (19 :: B.Word64), iv, pack [0x12, 0x13, 0x14]]
+      validateAuthSecret conf time message `shouldBe` False
+    it "should not validate if tokens do not match" $ do
+      time <- Clock.getCurrentTime
+      let encrypted = encrypt $ TokenStruct (time `addTime` 1000) $ Token $ pack [2..33]
+      let message = mconcat [tokenString, B.encode (fromIntegral $ length encrypted :: B.Word64), iv, encrypted]
+      validateAuthSecret conf time message `shouldBe` False
+    it "should not validate if secret is expired" $ do
+      time <- Clock.getCurrentTime
+      let encrypted = encrypt $ TokenStruct (time `addTime` 8000) $ Token tokenString
+      let message = mconcat [tokenString, B.encode (fromIntegral $ length encrypted :: B.Word64), iv, encrypted]
+      validateAuthSecret conf time message `shouldBe` False
+    it "should not validate secrets" $ do
+      time <- Clock.getCurrentTime
+      let encrypted = encrypt $ TokenStruct (time `addTime` 4000) $ Token tokenString
+      let message = mconcat [tokenString, B.encode (fromIntegral $ length encrypted :: B.Word64), iv, encrypted]
+      validateAuthSecret conf time message `shouldBe` False
