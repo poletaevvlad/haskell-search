aa0ae34c43589479c0396d7980119614ee7858d3
Vlad Poletaev <poletaev.vladislav@gmail.com>
2019-11-26 23:16:31 +0300

    Displaying editor form for creating and updating documents.

--- src/Pages/AdminPages.hs
+++ src/Pages/AdminPages.hs
@@ -3,7 +3,8 @@ module Pages.AdminPages (adminHandler) where
 import Control.Monad(msum)
 import Happstack.Server (ok, ServerPart, Response, toResponse, tempRedirect,
   dir, askRq, require, look, getDataFn, rqMethod, decodeBody, Method(POST),
-  defaultBodyPolicy, mkCookie, CookieLife(MaxAge), addCookie, expireCookie)
+  defaultBodyPolicy, mkCookie, CookieLife(MaxAge), addCookie, expireCookie,
+  path, nullDir)
 import Presentation.Layout(appLayout, paginator)
 import Presentation.Login(loginForm)
 import Presentation.DocViews(smallDocumentsList)
@@ -12,9 +13,12 @@ import Pages.Auth (AuthConf(auConfTimeOut), checkPassword, generateAuthSecret)
 import TextUtils.Processing(hexEncode)
 import Data.Time.Clock (nominalDiffTimeToSeconds)
 import Pages.Auth (requireLogin, isLoggedIn)
-import Database.Documents(Document(getDocId))
+import Database.Documents(Document(getDocId, getDocName))
 import Pages.DocumentsIndex(optPageNum, PageNumber(..))
 import qualified Database.DocumentsDB as DB
+import Presentation.DocEditor
+import Data.Maybe
+import TextUtils.Editing(toEditor)
 
 handleLoginForm :: AuthConf -> ServerPart Response
 handleLoginForm conf = do
@@ -58,7 +62,7 @@ handleAdminPage pageNumber db =
       smallDocumentsList urlFormatter docs (50 * (fromPageNumber pageNumber - 1) + 1)
       paginator pageUrlFormatter (fromPageNumber pageNumber) $ pagesCount total
   where
-    urlFormatter doc = "/admin/edit?id=" ++ (show $ getDocId doc)
+    urlFormatter doc = "/admin/edit/" ++ (show $ getDocId doc)
     pageUrlFormatter 1 = "/admin"
     pageUrlFormatter n = "/admin/page-" ++ show n
     range = DB.paginationRange 50 $ fromPageNumber pageNumber
@@ -67,9 +71,29 @@ handleAdminPage pageNumber db =
     pagesCount total = ceiling $ fromIntegral total / (50 :: Double)
 
 
+handleEditPage :: Maybe (Document, [String]) -> ServerPart Response
+handleEditPage mdoc = do
+  let editorType = if isNothing mdoc then Create else Edit
+  let (title, content) = if isNothing mdoc then ("", "") else let Just (doc, docContents) = mdoc in (getDocName doc, toEditor docContents 80)
+  ok $ toResponse $ appLayout "Editing" "" $ do
+    docEditor editorType title content
+
+
 adminHandler :: AuthConf -> DB.Database -> ServerPart Response
 adminHandler authConf db = msum [
   dir "admin" $ dir "login" $ handleLoginForm authConf,
   dir "admin" $ dir "logout" $ handleLogout,
-  dir "admin" $ optPageNum $ \pageNum -> requireLogin authConf >> handleAdminPage pageNum db
+  dir "admin" $ optPageNum $ \pageNum -> requireLogin authConf >> handleAdminPage pageNum db,
+  dir "admin" $ dir "edit" $ path $ \docId -> require (loadDocForEditing docId) $ \doc -> requireLogin authConf >> handleEditPage (Just doc),
+  dir "admin" $ dir "edit" $ nullDir >> requireLogin authConf >> handleEditPage Nothing
   ]
+
+  where
+    loadDocForEditing :: Int -> IO (Maybe (Document, [String]))
+    loadDocForEditing docId = do
+      mdoc <- DB.getDocumentById db docId
+      case mdoc of
+        Nothing -> return Nothing
+        Just doc -> do
+          content <- DB.getDocumentContent db doc
+          return $ Just (doc, content)
