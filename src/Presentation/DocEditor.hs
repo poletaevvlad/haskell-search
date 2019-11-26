e897412d9424a81b4fbd571723eddaad36ad8ee4
Vlad Poletaev <poletaev.vladislav@gmail.com>
2019-11-26 19:02:24 +0300

    Implemented document editing form.

---
+++ src/Presentation/DocEditor.hs
@@ -0,0 +1,23 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Presentation.DocEditor(docEditor, EditorType(..)) where
+
+import Text.Blaze ((!))
+import Text.Blaze(toValue)
+import qualified Text.Blaze.Html5 as H
+import qualified Text.Blaze.Html5.Attributes as A
+
+
+data EditorType = Create | Edit
+  deriving (Eq)
+
+
+docEditor :: EditorType -> String -> String -> H.Html
+docEditor edType title text = do
+  H.form ! A.class_ "edit-form" ! A.action "POST" $ do
+    H.div ! A.class_ "form-row" $ do
+      H.label "Title:" ! A.for "title"
+      H.input ! A.type_ "text" ! A.name "title" ! A.value (toValue title)
+    H.textarea (H.toHtml text) ! A.name "text"
+    let btnText = if edType == Create then "[ Publish ]" else "[ Update ]"
+    H.input ! A.type_ "submit" ! A.value btnText
---
+++ tests/Presentation/DocEditorSpec.hs
@@ -0,0 +1,32 @@
+module Presentation.DocEditorSpec(spec) where
+
+import Test.Hspec
+import Presentation.DocEditor
+import Text.Blaze.Html.Renderer.String
+
+spec :: Spec
+spec = do
+  describe "docEditor" $ do
+    it "should generate form for document creation" $ do
+      let expected = "<form class=\"edit-form\" action=\"POST\">\
+          \<div class=\"form-row\">\
+            \<label for=\"title\">Title:</label>\
+            \<input type=\"text\" name=\"title\" value=\"\">\
+          \</div>\
+          \<textarea name=\"text\"></textarea>\
+          \<input type=\"submit\" value=\"[ Publish ]\">\
+        \</form>"
+      renderHtml (docEditor Create "" "") `shouldBe` expected
+
+    it "should generate form for document updating" $ do
+      let expected = "<form class=\"edit-form\" action=\"POST\">\
+          \<div class=\"form-row\">\
+            \<label for=\"title\">Title:</label>\
+            \<input type=\"text\" name=\"title\" value=\"Document Title\">\
+          \</div>\
+          \<textarea name=\"text\">Document content</textarea>\
+          \<input type=\"submit\" value=\"[ Update ]\">\
+        \</form>"
+      renderHtml (docEditor Edit "Document Title" "Document content") `shouldBe` expected
+
+
