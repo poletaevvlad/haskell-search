289cd24cdef5d8dfcb835445f52360e4b5646ed0
Vlad Poletaev <poletaev.vladislav@gmail.com>
2019-11-28 20:48:42 +0300

    Loading configuration on start

--- exe/Main.hs
+++ exe/Main.hs
@@ -2,18 +2,17 @@ module Main where
 
 import Control.Monad
 import Paths_webse
-import Happstack.Server (nullConf, simpleHTTP, serveDirectory, dir, askRq,
-                         Browsing(DisableBrowsing), trailingSlash, toResponse,
-                         rqUri, rqQuery, ServerPart, Response, tempRedirect,
-                         guardRq)
+import Happstack.Server (simpleHTTP, serveDirectory, dir, askRq, guardRq,
+  Browsing(DisableBrowsing), trailingSlash, toResponse, rqUri, rqQuery,
+  ServerPart, Response, tempRedirect)
 import Pages.DocumentPage(documentPageHandler)
 import Pages.DocumentsIndex(documentsIndexHandler)
 import Pages.AdminPages(adminHandler)
 import Database.DocumentsDB(loadDatabase)
 import Search.Index(loadIndex)
 import Data.IORef
-import Pages.Auth (AuthConf(..))
-import qualified Data.ByteString.Lazy as BS
+import System.Directory (getXdgDirectory, XdgDirectory(XdgConfig), createDirectoryIfMissing)
+import Service.Config (loadConfig)
 
 
 removeTrailingSlash :: ServerPart Response
@@ -27,23 +26,22 @@ removeTrailingSlash =
 
 main :: IO ()
 main = do
-  putStrLn "Launchig server"
-  static_dir <- getDataFileName "Static"
-  db <- loadDatabase "/data/text-db"
-  index <- loadIndex "/data/text-db"
-  indexRef <- newIORef $ Just index
-
-  let hash = [0x5e, 0x88, 0x48, 0x98, 0xda, 0x28, 0x04, 0x71,
-              0x51, 0xd0, 0xe5, 0x6f, 0x8d, 0xc6, 0x29, 0x27,
-              0x73, 0x60, 0x3d, 0x0d, 0x6a, 0xab, 0xbd, 0xd6,
-              0x2a, 0x11, 0xef, 0x72, 0x1d, 0x15, 0x42, 0xd8]
-  let authConf = AuthConf { auConfTimeOut = 24 * 60 * 60
-                          , auConfSecret = BS.pack [0..31]
-                          , auConfPasswordHash = BS.pack hash }
-
-  simpleHTTP nullConf $ msum [
-    removeTrailingSlash,
-    documentsIndexHandler db indexRef,
-    documentPageHandler db,
-    adminHandler authConf db indexRef,
-    dir "static" $ serveDirectory DisableBrowsing [] static_dir]
+  configPath <- getXdgDirectory XdgConfig "webse.ini"
+  config <- loadConfig configPath
+  case config of
+    Left errorMessage -> do
+      putStrLn $ "Configuration error:"
+      putStrLn $ "  config file location: " ++ configPath
+      putStrLn $ "  " ++ errorMessage
+    Right (netConfig, authConf, path) -> do
+      staticDir <- getDataFileName "Static"
+      createDirectoryIfMissing True path
+      db <- loadDatabase path
+      index <- loadIndex path
+      indexRef <- newIORef $ Just index
+      simpleHTTP netConfig $ msum [
+        removeTrailingSlash,
+        documentsIndexHandler db indexRef,
+        documentPageHandler db,
+        adminHandler authConf db indexRef,
+        dir "static" $ serveDirectory DisableBrowsing [] staticDir]
