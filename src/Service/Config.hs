4baceffccb5ea10d96cb2eed511f5bcb37c0ead0
Vlad Poletaev <poletaev.vladislav@gmail.com>
2019-11-28 15:31:37 +0300

    Parsing intervals for configs

--- src/Service/Config.hs
+++ src/Service/Config.hs
@@ -1,14 +1,37 @@
-module Service.Config (parsePort) where
+module Service.Config (parsePort, parseInterval) where
 
-import Data.Text
+import Data.Text (Text)
+import qualified Data.Text as Text
 import Text.Read (readMaybe)
+import Data.Char (isNumber)
+import Data.List (span)
 
 
 parsePort :: Text -> Either String Int
-parsePort text = case readMaybe $ unpack text of
+parsePort text = case readMaybe $ Text.unpack text of
   Nothing -> Left "Port number must be an integer"
   Just port ->
     if port < 0 || port > 0xFFFF
       then Left "Port number must be in range 1..65535"
       else Right port
 
+
+errorIf :: Bool -> String -> Either String ()
+errorIf True message = Left message
+errorIf False _ = Right ()
+
+
+parseInterval :: Text -> Either String Int
+parseInterval text = do
+  let (number, suffix) = span isNumber $ Text.unpack text
+  errorIf (null number) "Interval value must start with an integer"
+  mul <- parseSuffix suffix
+  return $ read number * mul
+  where
+    parseSuffix :: String -> Either String Int
+    parseSuffix "" = Right 1
+    parseSuffix "s" = Right 1
+    parseSuffix "m" = Right 60
+    parseSuffix "h" = Right $ 60 * 60
+    parseSuffix "d" = Right $ 60 * 60 * 24
+    parseSuffix unit = Left $ "Unknown unit: '" ++ unit ++ "'"
--- tests/Service/ConfigSpec.hs
+++ tests/Service/ConfigSpec.hs
@@ -20,3 +20,21 @@ spec = do
       (parsePort $ pack "8000000") `shouldBe` Left "Port number must be in range 1..65535"
     it "should accept port number" $ do
       (parsePort $ pack "8080") `shouldBe` Right 8080
+
+  describe "parseInterval" $ do
+    it "should not accept empty string" $ do
+      (parseInterval $ pack "") `shouldBe` Left "Interval value must start with an integer"
+    it "should not accept string starting with non-digits" $ do
+      (parseInterval $ pack "abc") `shouldBe` Left "Interval value must start with an integer"
+    it "should not accept string with unknown prefix" $ do
+      (parseInterval $ pack "145f") `shouldBe` Left "Unknown unit: 'f'"
+    it "should parse the number of seconds without suffix" $ do
+      (parseInterval $ pack "145") `shouldBe` Right 145
+    it "should parse the number of seconds" $ do
+      (parseInterval $ pack "18s") `shouldBe` Right 18
+    it "should parse the number of minutes" $ do
+      (parseInterval $ pack "10m") `shouldBe` Right 600
+    it "should parse the number of hours" $ do
+      (parseInterval $ pack "2h") `shouldBe` Right 7200
+    it "should parse the number of days" $ do
+      (parseInterval $ pack "4d") `shouldBe` Right 345600
